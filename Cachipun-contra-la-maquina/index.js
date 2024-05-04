059a561721d2a7df4610ed5c6465ceb13fa5b8d9
Angello Lopez <lopezaguayo@aol.com>
2024-05-04 01:04:08 -0400

    Solucion a desafio Cachipun contra la maquina

--- Cachipun-contra-la-maquina/index.js
+++ Cachipun-contra-la-maquina/index.js
@@ -1,37 +1,48 @@
 function jugarCachipun() {
-  var totalPartidas = prompt("¿Cuántas veces quieres jugar contra la computadora?");
-  totalPartidas = parseInt(totalPartidas);
+    var numeroPartidas = prompt("¿Cuántas veces quieres jugar contra la computadora?");
+    var contador = 0;
 
-  for (var i = 0; i < totalPartidas; i++) {
-      var eleccionUsuario = prompt("Elige tu jugada: piedra, papel o tijera");
-      eleccionUsuario = eleccionUsuario.toLowerCase();
-      var opciones = ['piedra', 'papel', 'tijera'];
-      var eleccionComputadora = opciones[Math.floor(Math.random() * 3)];
+    while (contador < parseInt(numeroPartidas)) {
+        var eleccionUsuario = prompt("Elige tu jugada: piedra, papel o tijera").toLowerCase();
+        var jugadaComputadora = Math.floor(Math.random() * 3);
+        var eleccionComputadora;
 
-      alert("La computadora eligió: " + eleccionComputadora);
+        if (jugadaComputadora === 0) {
+            eleccionComputadora = "piedra";
+        } else if (jugadaComputadora === 1) {
+            eleccionComputadora = "papel";
+        } else {
+            eleccionComputadora = "tijera";
+        }
 
-      if (eleccionUsuario === eleccionComputadora) {
-          alert("Es un empate.");
-      } else if (eleccionUsuario === "piedra") {
-          if (eleccionComputadora === "tijera") {
-              alert("¡Felicidades, has ganado :)!");
-          } else {
-              alert("Has perdido contra la máquina.");
-          }
-      } else if (eleccionUsuario === "papel") {
-          if (eleccionComputadora === "piedra") {
-              alert("¡Felicidades, has ganado :)!");
-          } else {
-              alert("Has perdido contra la máquina.");
-          }
-      } else if (eleccionUsuario === "tijera") {
-          if (eleccionComputadora === "papel") {
-              alert("¡Felicidades, has ganado :)!");
-          } else {
-              alert("Has perdido contra la máquina.");
-          }
-      } else {
-          alert("Parece que ingresaste una opción inválida.");
-      }
-  }
+        alert("La computadora eligió: " + eleccionComputadora);
+
+        if (eleccionUsuario === eleccionComputadora) {
+            alert("Es un empate.");
+        } else {
+            if (eleccionUsuario === "piedra") {
+                if (eleccionComputadora === "tijera") {
+                    alert("¡Felicidades, has ganado :) !");
+                } else {
+                    alert("Has perdido contra la máquina.");
+                }
+            } else if (eleccionUsuario === "papel") {
+                if (eleccionComputadora === "piedra") {
+                    alert("¡Felicidades, has ganado :) !");
+                } else {
+                    alert("Has perdido contra la máquina.");
+                }
+            } else if (eleccionUsuario === "tijera") {
+                if (eleccionComputadora === "papel") {
+                    alert("¡Felicidades, has ganado :) !");
+                } else {
+                    alert("Has perdido contra la máquina.");
+                }
+            } else {
+                alert("Esa no es una opción válida, elige piedra, papel o tijera.");
+                continue;
+            }
+        }
+        contador++;
+    }
 }
