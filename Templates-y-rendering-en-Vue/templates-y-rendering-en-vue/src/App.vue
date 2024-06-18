ef979034af2769e25b82ecd9b3e30b8076525b89
Angello Lopez <lopezaguayo@aol.com>
2024-06-18 02:12:29 -0400

    Templates y rendering en Vue: Parte II

--- Templates-y-rendering-en-Vue/templates-y-rendering-en-vue/src/App.vue
+++ Templates-y-rendering-en-Vue/templates-y-rendering-en-vue/src/App.vue
@@ -9,6 +9,7 @@
             <label for="bgColor" class="form-label">Color de fondo</label>
             <select class="form-select" aria-label="Default select example" v-model="bgColor">
               <option v-for="color in bgColors" :key="color" :value="color">{{ color }}</option>
+              <!--<option v-for="(color, index) in bgColors" :key="index">{{ color }}</option>-->
             </select>
           </div>
           <div class="mb-3">
@@ -32,11 +33,12 @@
           <div class="mb-3">
             <label for="opinion" class="form-label">Tipografia</label>
             <select class="form-select" aria-label="Default select example" v-model="tipografia">
-              <option v-for="tipografia in tipografias" :key="tipografia.value" :value="tipografia.value">{{ tipografia.label }}</option>
+              <option v-for="tipografia in tipografias" :key="tipografia.value" :value="tipografia.value">{{
+                tipografia.label }}</option>
             </select>
           </div>
           <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="opaco">
+            <input type="checkbox" class="form-check-input" id="opaco" v-model="opaco">
             <label class="form-check-label" for="opaco">Opaco</label>
           </div>
 
@@ -45,24 +47,39 @@
             <div class="col-sm-9">
               <div class="d-flex gap-3">
 
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="tamanoLetra" value="Regular"
-                    v-model="tamanoLetra" />
-                  <label class="form-check-label fw-normal" for="m">
-                    Pequeño
+                <div class="form-check" v-for="tamano in fontSizes" :key="tamano.value">
+                  <input class="form-check-input" type="radio" :value="tamano.value" v-model="tamanoLetra"
+                    :id="`size-${index}`" />
+                  <label class="form-check-label fw-normal" :for="`size-${index}`">
+                    {{ tamano.label }}
                   </label>
                 </div>
 
+                <!--<div class="form-check" v-for="(tamano, index) in fontSizes" :key="index">
+                  <input class="form-check-input" type="radio" :value="tamano.value" v-model="tamanoLetra"
+                    :id="`size-${index}`" />
+                  <label class="form-check-label fw-normal" :for="`size-${index}`">
+                    {{ tamano.label }}
+                  </label>
+                </div>-->
+
+                <!--NOTA: :key="index": :key es una propiedad de Vue que ayuda a mantener la eficiencia en la renderización de elementos en listas. 
+                index es utilizado como la clave única para cada elemento iterado.-->
+
+                <!--<div class="form-check">
+                  <input class="form-check-input" type="radio" value="16px" v-model="tamanoLetra" />
+                  <label class="form-check-label fw-normal" for="m">Pequeño</label>
+                </div>
+
                 <div class="form-check">
-                  <input class="form-check-input" type="radio" name="tamanoLetra" value="Bien" v-model="tamanoLetra" />
+                  <input class="form-check-input" type="radio" value="32px" v-model="tamanoLetra" />
                   <label class="form-check-label fw-normal" for="f">Mediano</label>
                 </div>
 
                 <div class="form-check">
-                  <input class="form-check-input" type="radio" name="tamanoLetra" value="Muy Bien"
-                    v-model="tamanoLetra" />
+                  <input class="form-check-input" type="radio" value="64px" v-model="tamanoLetra" />
                   <label class="form-check-label fw-normal" for="f">Grande</label>
-                </div>
+                </div>-->
 
               </div>
             </div>
@@ -73,9 +90,10 @@
       </div><!--/ FORM-->
 
       <div class="d-flex justify-content-center align-items-center flex-grow-1 p-4">
-        <div class="w-75 h-75" v-show="mostrarTexto" :style="{ background: bgColor, color: textColor, borderRadius: borderRadius + 'px' } " ><!--FIGURE-->
-          <h1 :class="tipografia">{{ contenidoTextual }}</h1>
-          {{ tamanoLetra }}
+        <div class="w-75 h-75" v-show="mostrarTexto"
+          :style="{ background: bgColor, color: textColor, fontSize: tamanoLetra, borderRadius: borderRadius + 'px', opacity: opaco ? 0.5 : 1 }">
+          <!--FIGURE-->
+          <p class="text-break" :class="tipografia">{{ contenidoTextual }}</p>
         </div><!--/ FIGURE-->
       </div>
 
@@ -86,31 +104,35 @@
 
 <script>
 export default {
-  
+
   name: 'App',
   data() {
     return {
-      bgColor:"",
+      bgColor: "",
       bgColors: ["DarkGreen", "Yellow"], // 3 - Usar v-for para iterar un arreglo del estado en el template para la renderización dinámica de elementos.
-      textColor:"",
+      textColor: "",
       textColors: ["White", "Black"],
       mostrarTexto: true, // 2 -Usar class Binding con clases y valores boleanos. 4 - Usar v-show para mostrar u ocultar la figura.
       borderRadius: 0,
       contenidoTextual: "",
-      tipografia:"",
+      tipografia: "",
       tipografias: [
         { label: "Cursiva", value: "font-italic" },
         { label: "Negrita", value: "font-bold" }
       ],
-      opaco:"",
-      tamanoLetra:""
+      opaco: true,
+      fontSizes: [
+        { label: "Pequeña", value: "16px" },
+        { label: "Mediana", value: "32px" },
+        { label: "Grande", value: "64px" }
+      ],
+      tamanoLetra: ""
     }
   }
 }
 </script>
 
 <style>
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -124,11 +146,11 @@ export default {
   background: #2f4f4f;
 }
 
-.bg-darkgreen{
+.bg-darkgreen {
   background-color: #80b280;
 }
 
-.bg-palepink{
+.bg-palepink {
   background-color: #f5cac3;
 }
 
@@ -139,5 +161,4 @@ export default {
 .font-bold {
   font-weight: bold;
 }
-
 </style>
