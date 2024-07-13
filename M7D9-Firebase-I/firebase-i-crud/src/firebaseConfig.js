cddc7417499d462105a1063e3573abcbf7b3e0bc
Angello Lopez <lopezaguayo@aol.com>
2024-07-13 01:38:52 -0400

    M7D9 - Firebase (I): CRUD

---
+++ M7D9-Firebase-I/firebase-i-crud/src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB74MJcLy0hBC2iHELzKv8TU8fHxxNN3OY",
+  authDomain: "fir-crud-907f8.firebaseapp.com",
+  projectId: "fir-crud-907f8",
+  storageBucket: "fir-crud-907f8.appspot.com",
+  messagingSenderId: "354800243219",
+  appId: "1:354800243219:web:a41215b7577420d0cda5e6"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
